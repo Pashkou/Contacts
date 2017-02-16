a78be90329fece0c1a2040b28bcb1d28c4915f2c
Siarhei Pashkou <pashkou.s@gmail.com>
2017-02-16 07:32:20 +0100

    just add few testfiles

---
+++ org.eclipse.scout.contacts.client/src/test/java/org/eclipse/scout/contacts/client/DefaultIconProviderServiceTest.java
@@ -0,0 +1,13 @@
+package org.eclipse.scout.contacts.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DefaultIconProviderServiceTest {
+
+	@Test
+	public void test() {
+	}
+
+}
---
+++ org.eclipse.scout.contacts.client/src/test/java/org/eclipse/scout/contacts/client/contact/ContactOutlineTest.java
@@ -0,0 +1,13 @@
+package org.eclipse.scout.contacts.client.contact;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactOutlineTest {
+
+	@Test
+	public void test() {
+	}
+
+}
