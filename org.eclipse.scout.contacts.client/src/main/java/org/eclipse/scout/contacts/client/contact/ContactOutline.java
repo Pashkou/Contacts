bfe89b7483c9abc145a2f41a75495d8ce6177f1d
Siarhei Pashkou <pashkou.s@gmail.com>
2017-02-16 18:04:45 +0100

    add new columns

--- org.eclipse.scout.contacts.client/src/main/java/org/eclipse/scout/contacts/client/contact/ContactOutline.java
+++ org.eclipse.scout.contacts.client/src/main/java/org/eclipse/scout/contacts/client/contact/ContactOutline.java
@@ -2,6 +2,7 @@ package org.eclipse.scout.contacts.client.contact;
 
 import java.util.List;
 
+import org.eclipse.scout.contacts.client.person.PersonTablePage;
 import org.eclipse.scout.contacts.shared.Icons;
 import org.eclipse.scout.rt.client.ui.desktop.outline.AbstractOutline;
 import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
@@ -18,6 +19,7 @@ public class ContactOutline extends AbstractOutline {
 
 	@Override
 	protected void execCreateChildPages(List<IPage<?>> pageList) {
+		 pageList.add(new PersonTablePage());
 	}
 
 	@Override
--- org.eclipse.scout.contacts.client/src/main/java/org/eclipse/scout/contacts/client/person/PersonTablePage.java
+++ org.eclipse.scout.contacts.client/src/main/java/org/eclipse/scout/contacts/client/person/PersonTablePage.java
@@ -1,14 +1,19 @@
 package org.eclipse.scout.contacts.client.person;
 
+import org.eclipse.scout.contacts.client.common.CountryLookupCall;
 import org.eclipse.scout.contacts.client.person.PersonTablePage.Table;
 import org.eclipse.scout.contacts.shared.person.IPersonService;
 import org.eclipse.scout.contacts.shared.person.PersonTablePageData;
 import org.eclipse.scout.rt.client.dto.Data;
 import org.eclipse.scout.rt.client.ui.basic.table.AbstractTable;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.AbstractSmartColumn;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.AbstractStringColumn;
 import org.eclipse.scout.rt.client.ui.desktop.outline.pages.AbstractPageWithTable;
 import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.Order;
 import org.eclipse.scout.rt.shared.TEXTS;
 import org.eclipse.scout.rt.shared.services.common.jdbc.SearchFilter;
+import org.eclipse.scout.rt.shared.services.lookup.ILookupCall;
 
 @Data(PersonTablePageData.class)
 public class PersonTablePage extends AbstractPageWithTable<Table> {
@@ -29,5 +34,181 @@ public class PersonTablePage extends AbstractPageWithTable<Table> {
 	}
 
 	public class Table extends AbstractTable {
+
+
+		public PersonIdColumn getPersonIdColumn() {
+			return getColumnSet().getColumnByClass(PersonIdColumn.class);
+		}
+
+		public FirstNameColumn getFirstNameColumn() {
+			return getColumnSet().getColumnByClass(FirstNameColumn.class);
+		}
+
+		public LastNameColumn getLastNameColumn() {
+			return getColumnSet().getColumnByClass(LastNameColumn.class);
+		}
+
+		public CityColumn getCityColumn() {
+			return getColumnSet().getColumnByClass(CityColumn.class);
+		}
+		
+		public CountryColumn getCountryColumn() {
+			return getColumnSet().getColumnByClass(CountryColumn.class);
+		}
+		
+		public PhoneColumn getPhoneColumn() {
+			return getColumnSet().getColumnByClass(PhoneColumn.class);
+		}
+		
+		public MobileColumn getMobileColumn() {
+			return getColumnSet().getColumnByClass(MobileColumn.class);
+		}
+		
+		public EmailColumn getEmailColumn() {
+			return getColumnSet().getColumnByClass(EmailColumn.class);
+		}
+
+
+		@Order(1)
+		public class PersonIdColumn extends AbstractStringColumn {
+			@Override
+			protected String getConfiguredHeaderText() {
+				return TEXTS.get("MyNlsKey");
+			}
+
+			@Override
+			protected int getConfiguredWidth() {
+				return 100;
+			}
+			
+			@Override 
+		    protected boolean getConfiguredDisplayable() {
+				return false;
+		    }
+
+			@Override 
+			protected boolean getConfiguredPrimaryKey() {
+		        return true;
+		    }
+		}
+		
+		@Order(2)
+	    public class FirstNameColumn extends AbstractStringColumn {
+
+	      @Override
+	      protected String getConfiguredHeaderText() {
+	        return TEXTS.get("FirstName");
+	      }
+
+	      @Override
+	      protected int getConfiguredWidth() {
+	        return 120;
+	      }
+	    }
+		
+		@Order(3)
+	    public class LastNameColumn extends AbstractStringColumn {
+
+	      @Override
+	      protected String getConfiguredHeaderText() {
+	        return TEXTS.get("LastName");
+	      }
+
+	      @Override
+	      protected int getConfiguredWidth() {
+	        return 120;
+	      }
+	    }
+
+		@Order(4)
+		public class CityColumn extends AbstractStringColumn {
+			
+			@Override
+			protected String getConfiguredHeaderText() {
+				return TEXTS.get("City");
+			}
+			
+			@Override
+			protected int getConfiguredWidth() {
+				return 120;
+			}
+		}
+
+		@Order(5)
+		public class CountryColumn extends AbstractSmartColumn<String> {
+			@Override
+			protected String getConfiguredHeaderText() {
+				return TEXTS.get("Country");
+			}
+
+			@Override
+			protected int getConfiguredWidth() {
+				return 100;
+			}
+			
+			@Override 
+		    protected Class<? extends ILookupCall<String>> getConfiguredLookupCall() {
+		        return CountryLookupCall.class;
+		    }
+		}
+		
+		@Order(6)
+	    public class PhoneColumn extends AbstractStringColumn {
+
+	      @Override
+	      protected String getConfiguredHeaderText() {
+	        return TEXTS.get("Phone");
+	      }
+
+	      @Override 
+	      protected boolean getConfiguredVisible() {
+	        return false;
+	      }
+
+	      @Override
+	      protected int getConfiguredWidth() {
+	        return 120;
+	      }
+	    }
+		
+		@Order(7)
+		public class MobileColumn extends AbstractStringColumn {
+			
+			@Override
+			protected String getConfiguredHeaderText() {
+				return TEXTS.get("Mobile");
+			}
+			
+			@Override 
+			protected boolean getConfiguredVisible() {
+				return false;
+			}
+			
+			@Override
+			protected int getConfiguredWidth() {
+				return 120;
+			}
+		}
+		
+		@Order(8)
+		public class EmailColumn extends AbstractStringColumn {
+			
+			@Override
+			protected String getConfiguredHeaderText() {
+				return TEXTS.get("Email");
+			}
+			
+			@Override 
+			protected boolean getConfiguredVisible() {
+				return false;
+			}
+			
+			@Override
+			protected int getConfiguredWidth() {
+				return 120;
+			}
+		}
+		
+		
 	}
 }
