c010e0d35db67ddb62fdcb67aa3641b3b9163d33
Sergei <Sergei@DESKTOP-6TKRI3M>
2017-02-16 06:25:46 +0100

    initial checkin

---
+++ org.eclipse.scout.contacts.shared/src/main/java/org/eclipse/scout/contacts/shared/text/DefaultTextProviderService.java
@@ -0,0 +1,17 @@
+package org.eclipse.scout.contacts.shared.text;
+
+import org.eclipse.scout.rt.platform.Order;
+import org.eclipse.scout.rt.shared.services.common.text.AbstractDynamicNlsTextProviderService;
+
+/**
+ * <h3>{@link DefaultTextProviderService}</h3>
+ *
+ * @author Sergei
+ */
+@Order(-2000)
+public class DefaultTextProviderService extends AbstractDynamicNlsTextProviderService {
+	@Override
+	protected String getDynamicNlsBaseName() {
+		return "org.eclipse.scout.contacts.shared.texts.Texts";
+	}
+}
